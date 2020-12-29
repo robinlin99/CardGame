92113fa10d3dcd885170706d391fce0c71b169f8
Robin Lin <robin.lin99@yahoo.com>
2020-12-28 19:07:38 -0500

    Initial Commit

---
+++ CardGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardGame
+//
+//  Created by Robin Lin on 2020-12-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
